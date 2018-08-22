a07b0dc051f5ca27b39565beb169b035cdfa9a8e
Emily Wheeler <emily@Emilys-MacBook-Pro.local>
2018-08-22 16:07:32 -0700

    aperature size=7 0.1% contour and 7,7 blur

--- find_squares.py
+++ find_squares.py
@@ -13,19 +13,19 @@ def angle_cos(p0, p1, p2):
     return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
 
 def find_squares(img):
-    img = cv.GaussianBlur(img, (5, 5), 0)
+    img = cv.GaussianBlur(img, (7, 7), 0)
     squares = []
     for gray in cv.split(img):
         for thrs in xrange(0, 255, 26):
             if thrs == 0:
-                bin = cv.Canny(gray, 0, 50, apertureSize=5)
+                bin = cv.Canny(gray, 0, 50, apertureSize=7)
                 bin = cv.dilate(bin, None)
             else:
                 _retval, bin = cv.threshold(gray, thrs, 255, cv.THRESH_BINARY)
             bin, contours, _hierarchy = cv.findContours(bin, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
             for cnt in contours:
                 cnt_len = cv.arcLength(cnt, True)
-                cnt = cv.approxPolyDP(cnt, 0.02*cnt_len, True)
+                cnt = cv.approxPolyDP(cnt, 0.1*cnt_len, True)
                 if len(cnt) == 4 and cv.contourArea(cnt) > 1000 and cv.isContourConvex(cnt):
                     cnt = cnt.reshape(-1, 2)
                     max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in xrange(4)])
