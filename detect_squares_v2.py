be24fba2069966f7707aad2a843fec13789bc20a
Emily Wheeler <emily@emilys-mbp.wireless-guest.unc.edu>
2018-10-19 13:47:18 -0400

    new version cleaned up

--- detect_squares_v2.py
+++ detect_squares_v2.py
@@ -5,6 +5,7 @@ from numpy import linalg as la
 import find_squares
 import cv2 as cv
 import os
+import glob
 
 
 # Define square lengths for different arrays. List is [max_length, min_length, uniform_length]
@@ -13,7 +14,8 @@ side_lengths_dict = {"Air_100": [200, 150, 180],
                      "keyence_10x": [118, 80, 110],
                      "custom_100_20x_widefield": [350, 280, 310],
                      "custom_100_20x_confocal": [100, 70, 85],
-                     "cytosort_100_20x_confocal": [90, 60, 85]}
+                     "cytosort_100_20x_confocal": [90, 60, 85],
+                     "custom_100_old_10x_confocal": [220, 140, 170]}
 
 
 def load_image(filename):
@@ -178,5 +180,151 @@ def remove_overlapping_squares_v2(squares_dict, array_type):
     squares, df = make_uniform_squares(y_values.sort_values(by=['x_groups', 'y_groups']), array_type=array_type)
 
     return squares, df
+
+
+def master_one_img_gridscan(filename, array_type=None, max_length_filter=None,
+                            min_length_filter=None, blur=False):
+    """
+
+    :param filename: filename (full path) to analyze
+    :param array_type: Options: (Air_100, Air_200, keyence_10x, custom_100_20x_widefield,
+                                custom_100_20x_confocal, cytosort_100_20x_confocal)
+    :param max_length_filter: Customize if array type is not specified above
+    :param min_length_filter: Customize if array type is not specified above
+    :param blur: Set to True if squares aren't being detected and need an additional blurring step
+    :return: dictionary of squares, red image, blue image, and df of x and y row pairings
+    """
+
+    img = load_image(filename)
+
+    if blur is True:
+        for count in range(10):
+            img = cv.blur(img, (7,7), 9)
+
+    squares = find_squares.find_squares(img)
+    lengths = get_side_lengths_of_all_squares(squares)
+    new_squares = filter_squares_on_side_length(lengths, squares, array_type=array_type,
+                                                max_length_filter = max_length_filter,
+                                                min_length_filter = min_length_filter)
+
+    final_squares, df = remove_overlapping_squares_v2(new_squares, array_type)
+
+    red_file = filename.replace("2.tif", "1.tif")
+    blue_file = filename.replace("2.tif", "0.tif")
+
+    red_img = load_image(red_file)
+    blue_img = load_image(blue_file)
+
+    return final_squares, img, red_img, blue_img, df
+
+
+def extract_array_from_image(square, image):
+    """
+
+    :param square: numpy array of square coordinates
+    :param image: whole image to extract square from
+    :return: image from just the area of interest
+    """
+    x_and_y = gen.get_x_and_y_coords(square)
+
+    min_x = min(x_and_y[0])
+    max_x = max(x_and_y[0])
+    min_y = min(x_and_y[1])
+    max_y = max(x_and_y[1])
+
+    img_array = image[min_y:max_y, min_x:max_x]
+
+    return img_array
+
+
+def find_wells_with_cells(blue_dict, red_dict):
+    """
+
+    :param blue_dict: Dictionary of blue images per square
+    :param red_dict: Dictionary of red images per square
+    :return: wells with cells that pass thresholding cutoffs
+    """
+
+    blues_dict = dict()
+    reds_dict = dict()
+
+    for key in blue_dict.keys():
+        blue = blue_dict[key].sum().sum()
+        red = red_dict[key].sum().sum()
+
+        blues_dict[key] = blue
+        reds_dict[key] = red
+
+    blues = list(blues_dict.values())
+    reds = list(reds_dict.values())
+    blue_cutoff = np.mean(blues) + (0.1*np.std(blues))
+    red_cutoff = np.mean(reds) + (0.1*np.std(reds))
+
+    wells_with_cells = []
+
+    for key in blue_dict.keys():
+        if (blues_dict[key] > blue_cutoff) & (reds_dict[key] > red_cutoff):
+            wells_with_cells.append(key)
+
+    return wells_with_cells
+
+
+def get_dict_of_all_squares(bf_imgs, array_type, blur=False, char_to_split_name='.oir'):
+    """
+
+    :param bf_imgs: list of brightfield images
+    :param array_type: Options: (Air_100, Air_200, keyence_10x, custom_100_20x_widefield,
+                                custom_100_20x_confocal, cytosort_100_20x_confocal)
+    :param blur: Set to True if needed to blur images for bf detection (default=False)
+    :param char_to_split_name: for file naming. (default = '.oir')
+    :return: red_dict, blue_dict of all squares
+    """
+
+    red_dict = dict()
+    blue_dict = dict()
+
+    for image in bf_imgs:
+        name = os.path.basename(image).split(char_to_split_name)[0]
+        final_squares, img, red_img, blue_img, df = master_one_img_gridscan(image,
+                                                                            array_type=array_type, blur=blur)
+
+        number = 1
+
+        for square in final_squares.keys():
+            one = final_squares[square]
+            red = extract_array_from_image(one, red_img)
+            blue = extract_array_from_image(one, blue_img)
+
+            red_dict[name+"-"+str(number)] = red
+            blue_dict[name+"-"+str(number)] = blue
+
+            number += 1
+
+    return red_dict, blue_dict
+
+
+def process_all(data_dir, array_type, save_dir, bf_suffix='*2.tif', blur=False, char_to_split_name='.oir'):
+    """
+
+    :param data_dir: data with tif files
+    :param array_type: Options: (Air_100, Air_200, keyence_10x, custom_100_20x_widefield,
+                                custom_100_20x_confocal, cytosort_100_20x_confocal)
+    :param save_dir: directory where candidate_wells will get saved
+    :param bf_suffix: common suffix on all bf images with wildcard character (default = "*2.tif")
+    :param blur: Set to True if needed to blur images for bf detection (default=False)
+    :param char_to_split_name: for file naming. (default = '.oir')
+    :return: saves images in save_dir
+    """
+
+    bf_imgs = glob.glob(data_dir+bf_suffix)
+    red_dict, blue_dict = get_dict_of_all_squares(bf_imgs, array_type, blur, char_to_split_name)
+    wells_with_cells = find_wells_with_cells(blue_dict, red_dict)
+
+    blue_to_save = gen.make_new_dict_of_squares(blue_dict, wells_with_cells)
+    red_to_save = gen.make_new_dict_of_squares(red_dict, wells_with_cells)
+
+    for square in blue_to_save.keys():
+        cv.imwrite(save_dir+str(square)+"_blue.tif", blue_to_save[square])
+        cv.imwrite(save_dir+str(square)+"_red.tif", red_to_save[square])
 
 
