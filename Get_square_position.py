f5429f5f99030d1934f82f811758822de6958c92
Emily Wheeler <emily@emilys-mbp.wireless-guest.unc.edu>
2018-10-22 15:06:47 -0400

    hardcoded data_dir, fix later

--- Get_square_position.py
+++ Get_square_position.py
@@ -56,17 +56,22 @@ def get_absolute_center(hit_filename, metadata_info, coords):
 
     x_center = (min(coords[0]) + max(coords[0]))/2
     y_center = (min(coords[1]) + max(coords[1]))/2
-    absolute_x = metadata_info.loc[(hit_filename.split('-')[0]+".oir").strip("MAX_")]['x_absolute']
-    absolute_y = metadata_info.loc[(hit_filename.split('-')[0]+".oir").strip("MAX_")]['y_absolute']
 
-    row = [hit_filename, x_center+absolute_x, y_center+absolute_y]
+    lookup = (hit_filename.split('-')[0]+".oir").strip("MAX_")
+
+    absolute_x = metadata_info.loc[lookup]['x_absolute']
+    absolute_y = metadata_info.loc[lookup]['y_absolute']
+
+    x_step = int(metadata_info.loc[lookup]['x_step'])
+    y_step = int(metadata_info.loc[lookup]['y_step'])
+
+    row = [hit_filename, x_center+absolute_x, y_center+absolute_y, x_step, y_step]
     return row
 
 
-def master(data_dir, hits, array_type, check=False):
+def master(hits, array_type, check=False):
     """
 
-    :param data_dir:
     :param hits: list of hits
     :param array_type:
     :param check: want to see images called? default is False
@@ -76,16 +81,19 @@ def master(data_dir, hits, array_type, check=False):
     rows = []
 
     for hit in hits:
-        filename = data_dir+"tif/"+hit.split('-')[0]+".oir - C=2.tif"
-        number = int(hit.split('-')[1])
+        raft_num = hit.split("_")[2]
+        well_number = int(hit.split('-')[1])
+
+        data_dir = "/Users/emily/Desktop/181019_{}_Cycle/".format(raft_num)
+        tif_file = data_dir+"tif/"+hit.split('-')[0]+".oir - C=2.tif"
 
         metadata_file = data_dir+'analysis/pos_info.csv'
         metadata = pd.read_csv(metadata_file, index_col=0)
 
-        final_squares, img, red_img, blue_img, df_unused = grid.master_one_img_gridscan(filename,
-                                                                                 array_type=array_type,
-                                                                                 blur=True)
-        coords = get_square_coords(final_squares, number)
+        final_squares, img, red_img, blue_img, df_unused = grid.master_one_img_gridscan(tif_file,
+                                                                                        array_type=array_type,
+                                                                                        blur=True)
+        coords = get_square_coords(final_squares, well_number)
 
         if check is True:
             check_correct_square(final_squares, hit, red_img, blue_img)
@@ -95,6 +103,6 @@ def master(data_dir, hits, array_type, check=False):
         rows.append(row)
 
     df = pd.DataFrame(rows)
-    df.columns = ['name','x_pos','y_pos']
+    df.columns = ['name', 'x_pos', 'y_pos', 'x_step', 'y_step']
     df.set_index('name', inplace=True)
     return df
